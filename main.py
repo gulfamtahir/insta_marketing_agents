d7c77d854ca71e9c373555b60dbe6de467a108d0
Muhammad Gulfam Tahir <muhammadgulfamtahir@Mac.fritz.box>
2025-04-21 22:51:02 +0200

    Create each Pydantic model with session state

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ from agno.models.openai import OpenAIChat
 from agno.tools.googlesearch import GoogleSearchTools
 from agno.team import Team
 from agno.workflow import Workflow ,RunEvent, RunResponse
-from typing import Dict, Iterator
+from typing import Dict, Iterator , List
 from agno.utils.pprint import pprint_run_response
-from textwrap import dedent
+from agno.utils.log import logger
 from textwrap import dedent
 from rich.pretty import pprint
 from dotenv import load_dotenv
-
+############# GETTING PYDANTIC MODELS ################
+from research_model import InstaArticles
+from content_model import ContentCalendar
+from copy_writer_model import WeeklyCopywritingOutput
 load_dotenv()
+
+
+
+
+
 class InstagramContentWorkflow(Workflow):
     market_researcher= Agent(
         name="Instagram Market Researcher",
         model=OpenAIChat("gpt-4o-mini"),
         tools=[GoogleSearchTools()],
+        add_datetime_to_instructions=True,
+        structured_outputs=True,
+        show_tool_calls=True,
         description=dedent("Armed with a keen eye for digital trends and a deep understanding of the Instagram landscape, you excel at uncovering actionable insights from social media data. Your analytical skills are unmatched, providing a solid foundation for strategic decisions in content creation. You are great at identifying the latest trends and the best hashtags for a given campaign.."),
         instructions=dedent("""\ 
                             Investigate the latest trends, hashtags, and competitor activities on Instagram specific to the industry of this Instagram account. Focus on gathering data that reveals what content performs well in the current year, identifying patterns, preferences, and emerging trends. 
@@ -24,12 +35,10 @@ class InstagramContentWorkflow(Workflow):
                             <INSTAGRAM_ACCOUNT_DESCRIPTION>{instagram_description}</INSTAGRAM_ACCOUNT_DESCRIPTION>
                             Find the most relevant topics, hashtags and trends to use the the posts for next week. The focus of the following week is the following: 
                             <NEXT_WEEK_CONTENT>{topic_of_the_week}</NEXT_WEEK_CONTENT> """),
-        expected_output=dedent("""\ A report with the most relevant information that you found, including relevant hashtags for this week's content and all other information that could be useful to the team working on content creation.""")
+        expected_output=dedent("""\ A report with the most relevant information that you found, including relevant hashtags for this week's content and all other information that could be useful to the team working on content creation."""),
+        # response_model=InstaArticles
     
     )
-    def run(self, page_topic, weekly_topic)-> Iterator[RunResponse]:
-        return self.market_researcher.run(f"Do market reserach of this site:Instagram {page_topic}for the {weekly_topic}", stream=True)
-    
     
     content_strategist = Agent(
         name="Instagram Content Strategist",
@@ -72,8 +81,50 @@ class InstagramContentWorkflow(Workflow):
         expected_output="A markdown document with detailed descriptions of the images that will be used for each Instagram post of the week. Each description should be concise and include the main elements, colors, mood, and any other relevant information that will guide the creation of the image. The descriptions should align with the content calendar and the identified trends."
     )
 
+    def run(self, page_topic : str, weekly_topic: str , use_cache:bool)-> Iterator[RunResponse]:
+        logger.info(f"Generating Instagram Report on: {page_topic}")
+        logger.info(f"use cache: {use_cache}")
+        return self.market_researcher.run(f"Do market reserach of this site:Instagram {page_topic}for the {weekly_topic}", stream=True)
+    
+    def add_report_to_cache(self , page_topic : str,  weekly_topic: str)->None:
+        logger.info(f"Caching the Instagram Topic report for the topic: {page_topic}")
+        self.session_state.setdefault("insta_topic", [])
+        self.session_state['insta_topic']['page_topic'] = weekly_topic
+        logger.info(f"Instagram report cached successfully for the topic: {page_topic}")
+    
+    def get_cached_insta_report(self, page_topic: str)-> str:
+        logger.info(f"checking cache for the report of the topic: {page_topic}")
+        cached_report = self.session_state.get('insta_topic' ,[]).get(page_topic)
 
-# Usage
+        if cached_report:
+            logger.info(f"Cache hit for the Insta topic: {page_topic}")
+        else:
+            logger.info(f"No cache blog post found for the topic: {page_topic}")
+        return cached_report
+    
+    def _get_search_results(self, insta_topic: str) -> str:
+        MAX_ATTEMPTS = 3
+        for attempt in range(MAX_ATTEMPTS) :
+            try:
+                logger.info(f"Attempt {attempt + 1}: Searching for articles on '{insta_topic}'")
+                searcher_response: RunResponse = self.market_researcher.run(insta_topic)
+                if(searcher_response is not None and searcher_response.content is not None ):
+                    article_count = len(searcher_response.content.articles)
+                    logger.info(
+                        f"Found {article_count} articles on attempt {attempt + 1}"
+                    )
+                    return searcher_response.content
+                else:
+                    logger.warning(
+                        f"Attempt {attempt + 1}/{MAX_ATTEMPTS} failed: Invalid response type"
+                    )
+            except Exception as e:
+                logger.warning(f"Attempt {attempt + 1} failed with error: {e}")
+        logger.error(f"Failed to retrieve search results for '{insta_topic}' after {MAX_ATTEMPTS} attempts") 
+        return None
+    def write_report(self , topic: str , search_results : List)->Iterator[RunResponse]:
+        pass
+# usage
 workflow = InstagramContentWorkflow()
 report_stream = workflow.run(page_topic="Castle", weekly_topic="Castle in Germany")
 pprint_run_response(report_stream, markdown=True)
